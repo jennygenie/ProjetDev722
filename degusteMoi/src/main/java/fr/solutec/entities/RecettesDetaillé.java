10e6c87297ac88a0be00b25fe306482491bb38f4
siwertzm <siwertzm@gmail.com>
2022-11-24 17:53:31 +0100

    recette

---
+++ degusteMoi/src/main/java/fr/solutec/entities/RecettesDetaillé.java
@@ -0,0 +1,24 @@
+package fr.solutec.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity @Data @NoArgsConstructor @AllArgsConstructor
+public class RecettesDetaillé {
+	
+	@Id @GeneratedValue
+	private Long id;
+	private String titre;
+	private String temps_preparation;
+	private String temps_cuisson;
+	private String liste_ingredients;
+	private String liste_preparation;
+	private String appellations_recommandees;
+	
+
+}
