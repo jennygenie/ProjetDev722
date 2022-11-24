d118e2fbe502bd76253b4e546bd62bbcb5e13196
Arthur <arthur.glandier@gmail.com>
2022-11-24 14:01:04 +0100

    Editing friendship system

--- degusteMoi/src/main/java/fr/solutec/DegusteMoiApplication.java
+++ degusteMoi/src/main/java/fr/solutec/DegusteMoiApplication.java
@@ -1,11 +1,16 @@
 package fr.solutec;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import fr.solutec.entities.Demande;
 import fr.solutec.entities.User;
+import fr.solutec.repository.DemandeRepository;
 import fr.solutec.repository.UserRepository;
 
 
@@ -14,6 +19,7 @@ public class DegusteMoiApplication implements CommandLineRunner {
 	
 	@Autowired
 	private UserRepository userRepos;
+	@Autowired private DemandeRepository demandeRepos;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DegusteMoiApplication.class, args);
@@ -21,18 +27,24 @@ public class DegusteMoiApplication implements CommandLineRunner {
 		System.out.println("le projet t'as peur");
 
 		System.out.println("lancement terminé");
-
+		
+		
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		/*User u1 = new User(null, "Siwertz", "Marius", "siwertzm", "123");
+
+		User u1 = new User(null, "Siwertz", "Marius", "siwertzm", "123");
 		User u2 = new User(null, "Bardin", "Marion", "mbardin", "456");
 		User u3 = new User(null, "Scoliege", "Paul", "poloscolo", "789");
 		userRepos.save(u1);
 		userRepos.save(u2);
-		userRepos.save(u3);*/
+		userRepos.save(u3);
+		
+		
+		Demande d1 = new Demande(null, u1, u2, false);
+		demandeRepos.save(d1);
+		
 	}
 
 }
--- degusteMoi/src/main/java/fr/solutec/rest/UserRest.java
+++ degusteMoi/src/main/java/fr/solutec/rest/UserRest.java
@@ -1,5 +1,7 @@
 package fr.solutec.rest;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
