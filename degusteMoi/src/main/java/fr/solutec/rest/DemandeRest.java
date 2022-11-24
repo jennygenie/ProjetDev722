ac167b0019c896605c194985e477ef57fce22385
Arthur <arthur.glandier@gmail.com>
2022-11-24 15:18:57 +0100

    Added friends functions

--- degusteMoi/src/main/java/fr/solutec/rest/DemandeRest.java
+++ degusteMoi/src/main/java/fr/solutec/rest/DemandeRest.java
@@ -3,6 +3,7 @@ package fr.solutec.rest;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -47,6 +48,16 @@ public class DemandeRest {
 			Demande d = od.get();
 			d.setValide(true);
 			demanderepos.save(d);
+			return true;
+		}
+		return false;
+	}
+	
+	@DeleteMapping ("demande/reject/{id}")
+	private boolean rejectDemande(@PathVariable Long id) {
+		Optional<Demande> od = demanderepos.findById(id);
+		if (od.isPresent()) {
+			demanderepos.deleteById(id);
 			return true;
 		}
 		return false;
