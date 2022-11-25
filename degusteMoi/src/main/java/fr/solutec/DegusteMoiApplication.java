e1505d0745686f8ae549de2813be08bd3958241a
siwertzm <siwertzm@gmail.com>
2022-11-25 11:00:01 +0100

    test

--- degusteMoi/src/main/java/fr/solutec/DegusteMoiApplication.java
+++ degusteMoi/src/main/java/fr/solutec/DegusteMoiApplication.java
@@ -1,16 +1,12 @@
 package fr.solutec;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import fr.solutec.entities.Demande;
 import fr.solutec.entities.User;
-import fr.solutec.repository.DemandeRepository;
 import fr.solutec.repository.UserRepository;
 
 
@@ -19,7 +15,6 @@ public class DegusteMoiApplication implements CommandLineRunner {
 	
 	@Autowired
 	private UserRepository userRepos;
-	@Autowired private DemandeRepository demandeRepos;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DegusteMoiApplication.class, args);
@@ -34,26 +29,13 @@ public class DegusteMoiApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		/*User u1 = new User(null, "Siwertz", "Marius", "siwertzm", "123");
+		User u1 = new User(null, "Siwertz", "Marius", "siwertzm", "123");
 		User u2 = new User(null, "Bardin", "Marion", "mbardin", "456");
 		User u3 = new User(null, "Scoliege", "Paul", "poloscolo", "789");
-		User u4 = new User(null, "aa", "aa", "aa", "aa");
 		userRepos.save(u1);
 		userRepos.save(u2);
+		
 		userRepos.save(u3);
-		userRepos.save(u4);
-		
-		
-
-		Demande d1 = new Demande(null, u1, u2, false);
-		demandeRepos.save(d1);
-
-		Demande d1 = new Demande(null, u1, u2, true);
-		Demande d2 = new Demande(null, u2, u3, true);
-		Demande d3 = new Demande(null, u2, u4, true);
-		demandeRepos.save(d1);
-		demandeRepos.save(d2);
-		demandeRepos.save(d3);*/
 		
 	}
 
