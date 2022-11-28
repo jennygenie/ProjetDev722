b334138b24331e1e5f721bfa082b9c5aae26e266
siwertzm <siwertzm@gmail.com>
2022-11-28 10:37:12 +0100

    recette favoris

--- degusteMoi/src/main/java/fr/solutec/DegusteMoiApplication.java
+++ degusteMoi/src/main/java/fr/solutec/DegusteMoiApplication.java
@@ -44,6 +44,7 @@ public class DegusteMoiApplication implements CommandLineRunner {
 		userRepos.save(u4);*/
 		
 		
+		
 	}
 
 	
--- degusteMoi/src/main/java/fr/solutec/rest/FavoriRest.java
+++ degusteMoi/src/main/java/fr/solutec/rest/FavoriRest.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +17,7 @@ import fr.solutec.entities.Favori;
 import fr.solutec.entities.User;
 import fr.solutec.repository.FavoriRepository;
 
-@RestController
+@RestController @CrossOrigin("*")
 public class FavoriRest {
 	
 	@Autowired
